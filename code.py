b14ba98349ce4b39ca3475edda3c16f18194f413
bazarow17 <bazarovim2010@yandex.ru>
2026-02-06 12:05:30 +0300

    Update code.py

--- code.py
+++ code.py
@@ -8,14 +8,12 @@ elif a == 0:
 
     n = random.randint(1, 12)
     b = [random.randint(0, 99) for _ in range(n)]
-d = b.copy()
 for _ in range(len(b)):
     c = max(d)
     del b[b.index(c)]
-    del d[d.index(c)]
-    b.append(c)
+    d.append(c)
     j=j+1
-print(b, j)
+print(d, j)
 
 
 
@@ -28,7 +26,6 @@ elif a == 0:
 
     n = random.randint(1, 12)
     b = [random.randint(0, 99) for _ in range(n)]
-d = b.copy()
 
 b.sort()
 print(b)
@@ -46,7 +43,6 @@ elif a == 0:
 
     n = random.randint(1, 12)
     b = [random.randint(0, 99) for _ in range(n)]
-d = b.copy()
 for i in range(len(b)-1):
     if b[i]>b[i+1]:
     j=j+1
