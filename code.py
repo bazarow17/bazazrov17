d3887e3905cd123e3cc99af973933b63b8bfb4d2
bazarow17 <bazarovim2010@yandex.ru>
2026-02-06 12:00:53 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,5 @@
 '''
-import random
+j = 0
 a = int(input(''))
 if a == 1:
     n = int(input())
@@ -13,9 +13,27 @@ for _ in range(len(b)):
     c = max(d)
     del b[b.index(c)]
     del d[d.index(c)]
-    print(b)
     b.append(c)
+    j=j+1
+print(b, j)
+
+
+
+import random
+a = int(input(''))
+if a == 1:
+    n = int(input())
+    b = list(map(int, input().split()))
+elif a == 0:
+
+    n = random.randint(1, 12)
+    b = [random.randint(0, 99) for _ in range(n)]
+d = b.copy()
+
+b.sort()
 print(b)
+
+
 
 
 
@@ -31,6 +49,7 @@ elif a == 0:
 d = b.copy()
 for i in range(len(b)-1):
     if b[i]>b[i+1]:
+    j=j+1
         b[i], b[i+1] = b[i+1], b[i]
-print(b)
+print(b, j)
 '''
