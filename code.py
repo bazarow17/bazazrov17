2678dfcd6c5df93e4e47f4d0ccc095d4943e85a7
bazarow17 <bazarovim2010@yandex.ru>
2026-02-06 13:18:21 +0300

    Update code.py

--- code.py
+++ code.py
@@ -5,23 +5,23 @@
    ищем максимальный элемент
    удаляем его из массива
    вставляем его в конец массива
+import random
 j = 0
 a = int(input(''))
+d = []
 if a == 1:
     n = int(input())
     b = list(map(int, input().split()))
 elif a == 0:
-
     n = random.randint(1, 12)
     b = [random.randint(0, 99) for _ in range(n)]
-for _ in range(len(b)):
-    c = max(d)
+k = 0
+for i in range(len(b)):
+    c = max(b)
     del b[b.index(c)]
-    d.append(c)
+    d.insert(k+i, c)
     j=j+1
 print(d, j)
-
-
 
 
 '''
