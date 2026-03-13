c8da26f462dbfc39a2353794fac8fc15117883cd
bazarow17 <bazarovim2010@yandex.ru>
2026-03-13 13:20:22 +0300

    Update code.py

--- code.py
+++ code.py
@@ -6,27 +6,31 @@
    удаляем их из массива
    вставляем их в конец и начало нового массива
 
-
 import random
-j = 0
-a = int(input(''))
-d = []
-if a == 1:
-    b = list(map(int, input().split()))
-elif a == 0:
+print('интерактивный режим-1')
+print('автономный режим-0')
+schetchik = 0
+rejim = int(input(''))
+otv = []
+if rejim == 1:
+    spisok = list(map(int, input().split()))
+elif rejim == 0:
     n = random.randint(1, 12)
-    b = [random.randint(0, 99) for _ in range(n)]
-k = 0
-for i in range(len(b)//2):
-    e = min(b)
-    if len(b)>=2:
-        c = max(b)
-        del b[b.index(c)]
-        d.insert(k+i, c)
-    del b[b.index(e)]
-    d.insert(len(d)-i, e)
-    j=j+1
-print(d, j)
+    spisok = [random.randint(0, 99) for _ in range(n)]
+ishod_spisok = spisok.copy()
+for i in range(len(spisok)//2):
+    minlist = min(spisok)
+    if len(spisok)>=2:
+        maxlist = max(spisok)
+        del spisok[spisok.index(minlist)]
+        otv.insert(i, minlist)
+    del spisok[spisok.index(maxlist)]
+    otv.insert(len(otv)-i, maxlist)
+    schetchik+=1
+print('исходный список-', ishod_spisok)
+print('отсортированный список-', otv)
+print('кол-во перестановок', schetchik)
+
 
 
 
@@ -38,46 +42,52 @@ print(d, j)
    удаляем его из массива
    вставляем его в конец массива
 import random
-j = 0
-a = int(input(''))
-d = []
-if a == 1:
-    b = list(map(int, input().split()))
-elif a == 0:
+print('интерактивный режим-1')
+print('автономный режим-0')
+schetchik = 0
+rejim = int(input(''))
+otv = []
+if rejim == 1:
+    spisok = list(map(int, input().split()))
+elif rejim == 0:
     n = random.randint(1, 12)
-    b = [random.randint(0, 99) for _ in range(n)]
-k = 0
-for i in range(len(b)):
-    c = max(b)
-    del b[b.index(c)]
-    d.insert(k+i, c)
-    j=j+1
-print(d, j)
+    spisok = [random.randint(0, 99) for _ in range(n)]
+ishod_spisok = spisok.copy()
+for i in range(len(spisok)):
+    minlist = max(spisok)
+    del spisok[spisok.index(minlist)]
+    otv.insert(i, minlist)
+    schetchik=schetchik+1
+print('исходный список-', ishod_spisok)
+print('отсортированный список-', otv)
+print('кол-во перестановок', schetchik)
 
 
 
 спрашиваем у пользователя по поводу режима
 запришиваем или создаем на рандом массив для работы
 повторяем действие n(длина массива) раз 
-   ищем максимальный элемент
-   удаляем его из массива
-   вставляем его в конец массива
+   сравниваем попарно элементов
+   меняем местами больший и меньший
 import random
-j = 0
-a = int(input(''))
-d = []
-if a == 1:
-    b = list(map(int, input().split()))
-elif a == 0:
-    n = random.randint(1, 12)
-    b = [random.randint(0, 99) for _ in range(n)]
-k = 0
-for i in range(len(b)):
-    c = max(b)
-    del b[b.index(c)]
-    d.insert(k+i, c)
-    j=j+1
-print(d, j)
+print('интерактивный режим-1')
+print('автономный режим-0')
+schetchik = 0
+regim = int(input(''))
+if regim == 1:
+   spisok = list(map(int, input().split()))
+elif regim == 0:
+   n = random.randint(1, 12)
+   spisok = [random.randint(0, 99) for _ in range(n)]
+ishod_spisok = spisok.copy()
+for i in range(len(spisok)):
+   for j in range(i+1, len(spisok)):
+      if spisok[i]>spisok[j]:
+         spisok[i], spisok[j] = spisok[j], spisok[i]
+         schetchik+=1
+print('исходный список-', ishod_spisok)
+print('отсортированный список-', spisok)
+print('кол-во перестановок', schetchik)
 
 
 
